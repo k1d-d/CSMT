6fc6d36dedfb8b07f9f68f9f7f7baa6d4e50a0ab
k1d-d <coderop10@gmail.com>
2023-12-03 17:31:23 +0000

    Added accent colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#111111',
+        secondary: '#191919',
+        accent: '#D6C8B6',
+        disabled: '#D8D2CA',
+        correct: '#53DB31',
+        wrong: '#FF2F2',
+      },
+    },
   },
   plugins: [],
 }
-
